d8c10f2b8a40371faaa9453944de2c779b04fdfd
beardm4n <dmitriykorovko@gmail.com>
2019-07-04 14:09:04 +0300

    работа с формами

--- test-project/src/main.js
+++ test-project/src/main.js
@@ -1,12 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-
-Vue.directive('colored', ColorDirective);
-Vue.filter('uppercase', (value) => value.toUpperCase());
-
 new Vue({
-  //ES6
   render: h => h(App)
 
 }).$mount('#app')
