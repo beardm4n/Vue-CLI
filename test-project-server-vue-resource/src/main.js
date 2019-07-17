205c89a626c31550b6f1c7c0ad957b807e74172f
beardm4n <dmitriykorovko@gmail.com>
2019-07-17 13:14:46 +0300

    установил плагин vue-resource и настроил json-server

--- test-project-server-vue-resource/src/main.js
+++ test-project-server-vue-resource/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+//для того, чтобы исопльзовать плагин VueResource мы должны обратиться к глобальной переменной Vue и при помощи его метода use добавить данный плагин
+Vue.use(VueResource)
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
+
+//установим библотеку vue-resource, для создание запросов и боработки ответов с использованием XMLHttpRequest или JSONP
+//устновим сервер для разоработки. для этого используем локальный сервер - JSON-сервер. устанавливаем глобально (-g) и для локальной разработке утсанавливаем (--save-dev)
+//в package.json пропишем команду для запуска json-сервера
